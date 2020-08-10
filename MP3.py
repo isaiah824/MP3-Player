fed5e2f4eb7468310fbf5ce800dc36f28c15c569
Isaiah Wilson <isaiah824@live.com>
2020-08-10 14:15:28 -0400

    Foundation for mp3 player

---
+++ MP3.py
@@ -0,0 +1,17 @@
+import os
+from tkinter.filedialog import askdirectory
+import pygame
+from mutagen.id3 import ID3
+from tkinter import *
+
+#creates window and sets size
+root = Tk()
+root.minsize(300,300)
+
+listOfSongs = []
+songNames= []
+
+#
+v= StringVar()
+songLabel = Label(root, textvariable = v, width=35)
+
