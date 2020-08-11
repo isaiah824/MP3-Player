c86a7fdbb5dcbd4312e2da19f549cbe73ea8d302
Isaiah Wilson <isaiah824@live.com>
2020-08-10 22:34:55 -0400

    Completed Directory function as well as loading the first song

--- MP3.py
+++ MP3.py
@@ -14,4 +14,20 @@ songNames= []
 #
 v= StringVar()
 songLabel = Label(root, textvariable = v, width=35)
-#git test
+def chooseDirectory():
+    directory = askdirectory()
+    os.chdir(directory)
+
+    for file in os.listdir(directory):
+        if file.endswith(".mp3", ".flac", ".mp4"):
+            readir = os.path.realpath(file)
+
+            audio = ID3(readir)
+
+            songNames.append(audio['TUT2'].text[0]
+            listOfSongs.append(file)
+
+            pygame.mixer.init()
+
+            pygame.mixer.music.load(listOfSongs[0])
+
