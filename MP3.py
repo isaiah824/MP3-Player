0d0b2866c9ca321dadebeaf9d2335255ce7514f5
Isaiah Wilson <isaiah824@live.com>
2020-08-12 02:09:57 -0400

    Small fixes to functions to allow choosing directory to work correctly

--- MP3.py
+++ MP3.py
@@ -14,15 +14,14 @@ songNames= []
 #
 v= StringVar()
 songLabel = Label(root, textvariable = v, width=35)
-def chooseDirectory():
+def choosedirectory():
     directory = askdirectory()
     os.chdir(directory)
-
     for file in os.listdir(directory):
-        if file.endswith(".mp3", ".flac", ".mp4"):
-            readir = os.path.realpath(file)
+        if file.endswith(".mp3"):
+            realdir = os.path.realpath(file)
 
-            audio = ID3(readir)
+            audio = ID3(realdir)
 
             songNames.append(audio['TIT2'].text[0])
             listOfSongs.append(file)
@@ -30,60 +29,70 @@ def chooseDirectory():
             pygame.mixer.init()
 
             pygame.mixer.music.load(listOfSongs[0])
-
+choosedirectory()
 def updatelabel(event):
     global index
     global songname
     v.set(songNames[index])
 
-def nextSong(event):
+def playsong(event):
+    global index
+    index=0
+    pygame.mixer.music.play()
+    updatelabel(event)
+
+def nextsong(event):
     global index
     index += 1
     pygame.mixer.music.load(listOfSongs[index])
     pygame.mixer.music.play()
-    updatelabel()
+    updatelabel(event)
 
-def previousSong(event):
+def previoussong(event):
     global index
     index -= 1
     pygame.mixer.music.load(listOfSongs[index])
     pygame.mixer.music.play()
-    updatelabel()
+    updatelabel(event)
 
-def stopSong(event):
+def stopsong(event):
     pygame.mixer.music.stop()
     v.set("")
 
-def pauseSong():
+def pausesong(event):
     pygame.mixer.music.pause()
-    label=Label(root, text = 'Music Player')
-    label.pack()
+
+
 
 listbox=Listbox(root)
 listbox.pack()
 
 songNames.reverse()
 for items in songNames:
-    list.insert(0, items)
+    listbox.insert(0, items)
 
 songNames.reverse()
 
 nextButton =  Button(root, text='Next Song')
-nextButton.pack
+nextButton.pack()
+
+playButton =  Button(root, text='Play Song')
+playButton.pack()
 
 previousButton =  Button(root, text='Previous Song')
-previousButton.pack
+previousButton.pack()
 
 stopButton = Button(root,text='Stop Music')
 stopButton.pack()
 
 pauseButton = Button(root, text = 'Pause Music')
-pauseButton.pack
+pauseButton.pack()
 
-nextButton.bind("<Button-1>", nextSong)
-previousButton.bind("<Button-1>",previousSong)
-stopButton.bind("<Button-1>",stopSong)
-pauseButton.bind("<Button-1>",pauseSong)
+nextButton.bind("<Button-1>", nextsong)
+playButton.bind("<Button-1>", playsong)
+previousButton.bind("<Button-1>",previoussong)
+stopButton.bind("<Button-1>",stopsong)
+pauseButton.bind("<Button-1>",pausesong)
 
 songLabel.pack()
 
